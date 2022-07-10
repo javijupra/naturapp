278a16d840e0ef1715beb2c590470480bd915d2f
javijupra <javier.prado@hsc.utah.edu>
2022-07-10 15:20:18 -0600

    Testing new swiping

--- server.js
+++ server.js
@@ -10,18 +10,6 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '/naturaGAME_main.html'));
 });
 
-app.get("/main.css", (req, res) => {
-    res.sendFile(path.join(__dirname, '/main.css'));
-});
-
-app.get("/main.js", (req, res) => {
-    res.sendFile(path.join(__dirname, '/naturaGAME_main.js'));
-});
-
-app.get("/main.css.map", (req, res) => {
-    res.sendFile(path.join(__dirname, '/main.css.map'));
-});
-
 app.get("/manifest.webmanifest", (req, res) => {
     res.sendFile(path.join(__dirname, '/manifest.webmanifest'));
 });
